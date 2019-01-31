19377d78f109faaa8664830fb1a1001c6c41e505
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-01-31 12:57:15 -0300

    -#5003 Se agregaron mas casos a tener en cuenta y ahora tambien es configurable si se quiere solo el reporte de los metadatos inconsistentes o si se desea reportar y ademas arreglarlos
    Falta agregar los reportes

--- dspace-api/src/main/java/org/dspace/ctask/general/MetadataAuthorityCuration.java
+++ dspace-api/src/main/java/org/dspace/ctask/general/MetadataAuthorityCuration.java
@@ -7,20 +7,33 @@ import org.dspace.content.authority.Choices;
 import org.dspace.content.authority.factory.ContentAuthorityServiceFactory;
 import org.dspace.content.authority.service.ChoiceAuthorityService;
 import org.dspace.content.authority.service.MetadataAuthorityService;
+import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 import java.io.IOException;
 import java.util.List;
 
-public class MetadataAuthorityCuration extends AbstractCurationTask
-	{
+public class MetadataAuthorityCuration extends AbstractCurationTask {
 
 	    protected int status = Curator.CURATE_UNSET;
 	    private Item item;
 	    protected String result = null;
         protected MetadataAuthorityService metadataAuthorityService = ContentAuthorityServiceFactory.getInstance().getMetadataAuthorityService();
         protected ChoiceAuthorityService choiceAuthorityService = ContentAuthorityServiceFactory.getInstance().getChoiceAuthorityService();
+        
+        protected final String PLUGIN_PREFIX = "metadataauthority";
+        protected boolean fixmode = false;
+        protected boolean fixvariants = false;
 
+        @Override
+        public void init(Curator curator, String taskId) throws IOException
+        {
+            super.init(curator, taskId);
+            fixmode = configurationService.getBooleanProperty(PLUGIN_PREFIX + ".fix");
+            if (fixmode)
+            	fixvariants = configurationService.getBooleanProperty(PLUGIN_PREFIX + ".fixvariants");
+        }
+        
 	    @Override
 	    public int perform(DSpaceObject dso) throws IOException
 	    {
@@ -29,7 +42,7 @@ public class MetadataAuthorityCuration extends AbstractCurationTask
 	            item = (Item)dso;
 	            List<MetadataValue> values = itemService.getMetadata(item, Item.ANY, Item.ANY, Item.ANY, Item.ANY);
 	            for (MetadataValue value : values) {
-	                if(metadataAuthorityService.isAuthorityRequired(value.getMetadataField())){
+	                if(metadataAuthorityService.isAuthorityControlled(value.getMetadataField())){
 	                    if (value.getAuthority() == null) {
 	                        updateMetadataAuthority(value);
 	                    }
@@ -50,28 +63,46 @@ public class MetadataAuthorityCuration extends AbstractCurationTask
 	    }
 
 	    private void updateMetadataAuthority(MetadataValue value) {
-	        Choices choices = choiceAuthorityService.getBestMatch(value.getMetadataField().toString(), value.getValue(),item.getOwningCollection() ,null);
+	        Choices choices = choiceAuthorityService.getBestMatch(value.getMetadataField().toString(),value.getValue(),item.getOwningCollection() ,null);
 	        if (choices.total > 0) {
-	            value.setAuthority(choices.values[0].authority);
-	            if (value.getConfidence() != Choices.CF_ACCEPTED)
-	                value.setConfidence(Choices.CF_ACCEPTED);
+	        	String newAuthority = choices.values[0].authority;
+		        String label = choices.values[0].label;
+	        	if (label.equals(value.getValue())) {
+	        		//reportar
+	        		if (fixmode) {
+	        			value.setAuthority(newAuthority);
+	            		value.setConfidence(choices.confidence);
+	        		}
+	        	}
+	        	//si el label es distinto -> reportar
 	        }
 	        else {
-	            if (value.getConfidence() > Choices.CF_NOVALUE)
-	                value.setConfidence(Choices.CF_NOVALUE);
+	            if (value.getConfidence() != Choices.CF_NOTFOUND)
+	            	//reportar
+	            	if (fixmode)
+	            		value.setConfidence(Choices.CF_NOTFOUND);
 	        }
 	    }
 
 	    private void updateMetadataValue(MetadataValue value) {
 	        String label = choiceAuthorityService.getLabel(value.getMetadataField().toString(), value.getAuthority(),null);
-	        if (!label.isEmpty()) {
-	            if (!label.equals(value.getValue())){
-	                value.setValue(label);
-	            }
-	            if (value.getConfidence() != Choices.CF_ACCEPTED)
-	                value.setConfidence(Choices.CF_ACCEPTED);
+	       	if (label == null || label.isEmpty()) {
+	       		//reportar
+	       		if (fixmode)
+	       			value.setConfidence(Choices.CF_NOTFOUND);
+	       	}
+	       	else if (!label.equals(value.getValue())){
+	       			//reportar
+	        		if (fixvariants) {
+	        		   value.setValue(label);
+        			   value.setConfidence(Choices.CF_UNCERTAIN);
+	        		}
 	        }
+	        else if (value.getConfidence() < Choices.CF_UNCERTAIN) {
+	           		//reportar
+	           		if (fixmode)
+	        		   value.setConfidence(Choices.CF_UNCERTAIN);
+	           	}
 	    }
-
-	}
-
+	        
+}
