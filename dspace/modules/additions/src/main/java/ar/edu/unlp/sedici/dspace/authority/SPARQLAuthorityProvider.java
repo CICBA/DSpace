5bde91dba5e197b7077cebac583ffe90119dc640
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-02-26 11:45:18 -0300

    -#5022 Se normalizó la búsqueda con paréntesis en ChoiceAuthorities
    Al parsear parentesis en la búsqueda de tesauros, se rompía la clase com.hp.hpl.jena.sparql.lang.ParserSPARQL10.java.
    Por lo que se escaparon correctamente los paréntesis en las consultas sparql de las subclases de SPARQLAuthorityProvider. También al momento de realizar la consulta http a Drupal.

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/SPARQLAuthorityProvider.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/SPARQLAuthorityProvider.java
@@ -95,7 +95,7 @@ public abstract class SPARQLAuthorityProvider implements ChoiceAuthority {
 			int limit) {
 
 		parameterizedSparqlString.setParams(globalParameters);
-		Query query = QueryFactory.create(parameterizedSparqlString.toString(),
+		Query query = QueryFactory.create(normalizeTextForHttpQuery(parameterizedSparqlString),
 				this.getSPARQLSyntax());
 		query.setOffset(offset);
 
@@ -129,6 +129,17 @@ public abstract class SPARQLAuthorityProvider implements ChoiceAuthority {
 		return Syntax.syntaxSPARQL_10;
 	}
 
+	private String normalizeTextForHttpQuery(ParameterizedSparqlString parameterizedSparqlString) {
+		String query = parameterizedSparqlString.toString();
+		if (query.indexOf("\\(") >= 0) query = query.replace("\\(", "\\\\\\(");
+		if (query.indexOf("\\)") >= 0) query = query.replace("\\)", "\\\\\\)");
+		return query;
+	}
 
+	protected String normalizeTextForParserSPARQL10(String text) {
+		if (text.indexOf("(") >= 0) text = text.replace("(", "\\\\(");
+		if (text.indexOf(")") >= 0) text = text.replace(")", "\\\\)");
+		return text;
+	}
 
 }
