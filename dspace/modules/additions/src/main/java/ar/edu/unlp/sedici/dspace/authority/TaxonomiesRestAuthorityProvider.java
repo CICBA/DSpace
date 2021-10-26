8acf8c8c52e9748f816b0d2ed0f5ae26da9ca0a9
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2021-10-26 11:29:24 -0300

    ticket 7215 Migro los conectores REST de autoridades a la versión 7 de DSpace 	En Dspace 7 cambió la interfaz del ChoiceAuthority, reimplemento los conectores para que se adapten a la nueva interfaz 	Entre otras cosas ya no se pasa como parametro el field (el nombre del metadato), a los metodos getMatches, getBestMatch y getLabel 	Además cambio la configuración, ahora se configura un nombre de plugin de autoridad distinto por metadato

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TaxonomiesRestAuthorityProvider.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TaxonomiesRestAuthorityProvider.java
@@ -7,14 +7,14 @@ import org.dspace.content.authority.Choice;
 public class TaxonomiesRestAuthorityProvider extends RestAuthorityProvider {
 
 	@Override
-	protected Choice extractChoice(String field, Map<String, Object> singleResult, boolean searchById) {
-		String value = (String) singleResult.get(this.getFilterField(field));
-		String key = (String) singleResult.get(this.getIdField(field));
+	protected Choice extractChoice(Map<String, Object> singleResult, boolean searchById) {
+		String value = (String) singleResult.get(this.getFilterField());
+		String key = (String) singleResult.get(this.getIdField());
 		return new Choice(key, value, value);
 	}
 
     @Override
-    protected void addExtraQueryTextParams(String field, Map<String, String> params) {
+    protected void addExtraQueryTextParams(Map<String, String> params) {
         // TODO Auto-generated method stub
     }
 
